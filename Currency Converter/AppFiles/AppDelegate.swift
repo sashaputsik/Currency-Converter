08237f6f4d551e4ebb62bb81fc60324aa9dbd884
alex <sashaputsikwork@gmail.com>
2020-05-19 12:48:34 +0300

    Folder structure

--- Currency Converter/AppFiles/AppDelegate.swift
+++ Currency Converter/AppFiles/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Currency Converter
-//
-//  Created by Sasha Putsikovich on 19.05.2020.
-//  Copyright © 2020 Sasha Putsikovich. All rights reserved.
-//
-
 import UIKit
 import CoreData
 
