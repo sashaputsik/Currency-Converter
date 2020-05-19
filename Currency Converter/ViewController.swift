9c1f7fbb007fc2d024efb88dc09e2cbea0d8f16a
alex <sashaputsikwork@gmail.com>
2020-05-19 12:41:47 +0300

    Initial Commit

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by Sasha Putsikovich on 19.05.2020.
+//  Copyright © 2020 Sasha Putsikovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
